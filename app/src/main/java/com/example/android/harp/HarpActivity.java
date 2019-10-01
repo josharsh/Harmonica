65b62a71ffef9fcd6186649cd25148291e7c99dd
amin <whfk233@gmail.com>
2019-10-01 20:45:59 +0330

    Some optimization stuff git status!

--- app/src/main/java/com/example/android/harp/HarpActivity.java
+++ app/src/main/java/com/example/android/harp/HarpActivity.java
@@ -3,13 +3,13 @@ package com.example.android.harp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class HarpActivity extends AppCompatActivity {
 
-    public final static String MESSAGE_KEY ="M";
+    public final static String MESSAGE_KEY = "M";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +17,11 @@ public class HarpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_harp);
         Intent intent = getIntent();
         String message = intent.getStringExtra(MESSAGE_KEY);
-       // Log.v("H0000higgggggg",message);
-        TextView Vi=(TextView)findViewById(R.id.outputSecondView);
-        if(message!=""){
-            Toast.makeText(this,"HELLO", Toast.LENGTH_SHORT);}
+        // Log.v("H0000higgggggg",message);
+        TextView Vi = findViewById(R.id.outputSecondView);
+        if (!message.equals("")) {
+            Toast.makeText(this, "HELLO", Toast.LENGTH_SHORT).show();
+        }
         Vi.setText(message);
 
     }
