39d16d647b9d08a6298b2125f06a9f5e996d3cd9
Telekinesist <bjoernak@gmail.com>
2019-10-05 20:03:46 +0200

    Layout improvements to activity_harp
    - Changed text colour from blue to black
    - Added Copy to Clipboard button

--- app/src/main/java/com/example/android/harp/HarpActivity.java
+++ app/src/main/java/com/example/android/harp/HarpActivity.java
@@ -1,5 +1,8 @@
 package com.example.android.harp;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +16,17 @@ public class HarpActivity extends AppCompatActivity {
 
     public void goBackActivity(View view){
         finish();
+    }
+
+    public void copyToClipboard(View view){
+        TextView TV = findViewById(R.id.outputSecondView);
+        CharSequence output = TV.getText();
+        if (output == null){
+            return;
+        }
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("Numeric harmonica notation", output);
+        clipboard.setPrimaryClip(clip);
     }
 
     @Override
